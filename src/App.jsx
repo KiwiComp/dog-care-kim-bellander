68a150bb8206cb2af5a85eda6f0476dab43fdc54
KiwiComp <kim.bellander@gmail.com>
2025-09-24 15:28:00 +0200

    Basic layout for header, main content and footer. Tried to hide api key but didn't succeed. Therefore the key is not included in this push and will not be included until I have succeeded in hiding it.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,64 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Outlet } from 'react-router'
+import HeaderComponent from './COMPONENTS/HeaderComponent'
+import FooterComponent from './COMPONENTS/FooterComponent'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [dogList, setDogList] = useState([]);
+
+  useEffect(() => {
+    if(dogList.length === 0) {
+      fetchDogsFromApi();
+    }
+  }, [])
+
+  const fetchDogsFromApi = async () => {
+    console.log("1. Function executed.")
+    const baseUrl = "https://api.jsonbin.io/v3/b/";
+    const binId = "68cd197eae596e708ff3bcf1";
+    // const apiKey = import.meta.env.VITE_API_KEY.trim();
+    // const apiKey = `${process.env.REACT_APP_API_KEY}`
+    
+    // console.log("API Key:", apiKey);
+    console.log("[" + apiKey + "]");
+
+    const response = await fetch(`${baseUrl}${binId}`, {
+      method: 'GET',
+      headers: {
+         "X-Master-Key": apiKey
+      }
+    })
+    console.log("2. Response", response)
+
+    const data = await response.json();
+    console.log("3. Data", data)
+
+    setDogList(data.record.record);
+    console.log("4. Dog list", dogList)
+
+  }
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className='mainGrid'>
+
+      <header>
+        <HeaderComponent />
+      </header>
+
+      <section className='mainContent'>
+        <Outlet />
+      </section>
+
+      <footer className="footer">
+        <FooterComponent />
+      </footer>
+      
+    </main>
   )
 }
 
