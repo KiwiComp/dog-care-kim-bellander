7069a3906a108c3968cc11aa533e02b8d32dc5b8
KiwiComp <kim.bellander@gmail.com>
2025-09-24 16:57:49 +0200

    Layout and design for header, footer and startpage, both mobile and desktop. Fetch dogs from api and display four random dogs on startpage. api key still not included in this version, stil figuring out how to hide in sufficiently.

--- src/COMPONENTS/HeaderComponent.jsx
+++ src/COMPONENTS/HeaderComponent.jsx
@@ -1,16 +1,43 @@
 import { Link } from "react-router";
+import {useState} from 'react';
 import "../CSS/header-style.css"
 import companyLogo from "../assets/companyLogo.webp"
 
 
 function HeaderComponent() {
 
+    const [dropdownIsVisible, setDropdownIsVisible] = useState(false);
+
     return(
         <header className="header">
-            <Link to="/">
-                <img src={companyLogo} alt="small company logo that navigates to start page" style={{height: "3rem", width: "3rem"}} />
-            </Link>
-            <p>HEJ</p>
+
+
+            {/* DESKTOP ONLY */}
+            <section className="leftHeader desktopOnly">
+                <Link to="/">
+                    <img src={companyLogo} alt="small company logo that navigates to start page" style={{height: "3rem", width: "3rem"}} />
+                </Link>
+            </section>
+            <section className="rightHeader desktopOnly">
+                <Link to="/">Start Page</Link>
+            </section>
+            {/* DESKTOP ONLY */}
+
+
+            {/* MOBILE ONLY */}
+            <section className="mobileOnly">
+                <img src={companyLogo} alt="company logo" style={{height: "12rem", width: "12rem"}} />
+
+                <article className="menuMobileHeader">
+                    <button className="btnMenuHeader" onClick={() => setDropdownIsVisible(!dropdownIsVisible)}>Menu</button>
+
+                    <article className={dropdownIsVisible ? "dropdownMenu visible" : "dropdownMenu hidden"}>
+                        <Link to="/" onClick={() => setDropdownIsVisible(false)}>Start page</Link>
+                    </article>
+                </article>
+            </section>
+            {/* MOBILE ONLY */}
+
         </header>
     )
 }
